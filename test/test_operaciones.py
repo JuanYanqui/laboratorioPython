d6df55077adf6a284fb02b152e0339ea27bbdaa0
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2025-08-19 03:17:23 +0000

    Agregar anotaciones de tipo a los tests para mypy

--- test/test_operaciones.py
+++ test/test_operaciones.py
@@ -1,12 +1,12 @@
 import pytest
 from src.operaciones import suma, division
 
-def test_suma():
+def test_suma() -> None:
     assert suma(2, 3.0) == 5.0
 
-def test_division_normal():
+def test_division_normal() -> None:
     assert division(10, 2) == 5.0
 
-def test_division_cero():
+def test_division_cero() -> None:
     with pytest.raises(ValueError):
         division(10, 0)
